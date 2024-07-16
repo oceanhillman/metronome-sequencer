a48bc9d18832c2d33aa438a2c0b1976e87b388f0
Ocean Hillman <oceanhillman@gmail.com>
2024-07-16 00:15:50 -0400

    Implement playlist component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Metronome from "../components/Metronome"
+import Playlist from "@/components/Playlist";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Metronome />
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <Playlist />
     </main>
   );
 }
