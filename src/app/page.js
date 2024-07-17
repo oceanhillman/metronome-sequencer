0e3ab302a86d2aa6f691aa5b35aa54161d36a2b5
Ocean Hillman <oceanhillman@gmail.com>
2024-07-17 08:20:26 -0400

    Add editor component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
-import Metronome from "../components/Metronome"
-import Playlist from "@/components/Playlist";
+
+import Editor from "@/components/Editor";
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <Header />
-      <Playlist />
+      <Editor />
       <Footer />
     </main>
   );
