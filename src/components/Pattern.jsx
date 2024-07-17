07f3debf36b6f06f8fe88d65bf1ea3203ce9ac54
Ocean Hillman <oceanhillman@gmail.com>
2024-07-17 17:00:05 -0400

    Update with new inputs

--- src/components/Pattern.jsx
+++ src/components/Pattern.jsx
@@ -1,41 +1,68 @@
-import React, { forwardRef, useState } from 'react';
+import React, { forwardRef, useState, useEffect } from "react"
+import Image from "next/image"
+import QuantityInput from "@/components/QuantityInput"
+import NumberInput from "@/components/NumberInput"
+import DragHandleIcon from "/public/drag_handle.svg"
+import DeleteIcon from "/public/delete.svg"
+import CloneIcon from "/public/clone.svg"
 
 const Pattern = forwardRef((props, ref) => {
-    const { dataGrid, patternData, handleUpdatePattern, handleClickDelete } = props;
+    const { dataGrid, patternData, handleUpdatePattern, handleClickClone, handleClickDelete } = props;
 
-    const [inputData, setInputData] = useState(
-        {
+    const [inputData, setInputData] = useState({
             beatsPerMeasure: patternData.beatsPerMeasure,
             bpm: patternData.bpm,
             numMeasures: patternData.numMeasures,
         });
 
-    function handleUpdate(attribute, value) {
-        setInputData((prev) => ({
-            ...prev,
-            [attribute]: value,
-        }));
-        handleUpdatePattern(patternData.id, attribute, value)
-    }
+        function handleUpdate(attribute, value) {
+            setInputData((prev) => ({
+                ...prev,
+                [attribute]: value,
+            }));
+            handleUpdatePattern(patternData.id, attribute, value);
+        }
 
     return (
-        <div data-grid={{...dataGrid, isResizable: false}} key={ref} className="flex flex-row rounded-md h-[100px] w-full content-between justify-end bg-red-800 text-black font-roboto">
-        <p>Clone</p><p>Delete</p>
-            <div className="absolute w-full h-[75%] rounded-b-md bottom-0 bg-white/70">
-                <div className="flex">
-                    <div className="w-1/4">
-                        Beats per measure <input className="w-12" type="number" value={inputData.beatsPerMeasure} onChange={(e) => handleUpdate('beatsPerMeasure', Number(e.target.value))} />
+        <div data-grid={{...dataGrid, isResizable: false}} key={ref} className="handle cursor-move flex flex-row rounded-md h-[100px] w-full content-between justify-center items-start bg-blue-500 text-black">
+            <Image src={DragHandleIcon} alt="Drag handle icon" className="absolute w-auto h-auto"/>
+            <div className="flex h-[24px] items-center space-x-1 ml-auto mr-2">
+                <button onMouseDown={(event) => handleClickClone(event, patternData)} className="w-[20px] h-[20px]">
+                    <Image src={CloneIcon} alt="Clone icon" className="w-auto h-auto"/>
+                </button>
+                <button onMouseDown={(event) => handleClickDelete(event, patternData.id)} className="w-[20px] h-[20px]">
+                    <Image src={DeleteIcon} alt="Delete icon" className="w-auto h-auto"/>
+                </button>
+            </div>
+
+            <div className="no-drag cursor-auto absolute w-full h-[75%] rounded-b-md bottom-0 bg-gray-900">
+                <div className="grid grid-cols-3 grid-rows-2 font-roboto text-xl text-white">
+                    <div className="flex flex-col justify-center items-center col-span-1 row-span-2">
+                        <p>BPM</p>
+                        <NumberInput
+                            value={inputData.bpm}
+                            min={1}
+                            max={300}
+                            onChange={(e) => handleUpdate('bpm', Number(e.target.value))}
+                        />
                     </div>
-                    <div className="w-1/4">
-                        BPM: <input className="w-12" type="number" value={inputData.bpm} onChange={(e) => handleUpdate('bpm', Number(e.target.value))} />
+                    <div className="flex flex-col justify-center items-center col-span-1 row-span-2">
+                        <p>Beats</p>
+                        <NumberInput
+                            value={inputData.beatsPerMeasure}
+                            min={1}
+                            max={64}
+                            onChange={(e) => handleUpdate('beatsPerMeasure', Number(e.target.value))}
+                        />
                     </div>
-                    <div className="w-1/4">
-                        x<input className="w-12" type="number" value={inputData.numMeasures} onChange={(e) => handleUpdate('numMeasures', Number(e.target.value))} /> Measures
-                    </div>
-                    <div className="w-1/4">
-                        <button onMouseDown={(event) => handleClickDelete(event, patternData.id)} className="bg-slate-700 text-white text-sm px-1 py-1 rounded">
-                            x
-                        </button>
+                    <div className="flex flex-col justify-center items-center col-span-1 row-span-2">
+                        <p>Measures</p>
+                        <NumberInput
+                            value={inputData.numMeasures}
+                            min={1}
+                            max={64}
+                            onChange={(e) => handleUpdate('numMeasures', Number(e.target.value))}
+                        />
                     </div>
                 </div>
             </div>
