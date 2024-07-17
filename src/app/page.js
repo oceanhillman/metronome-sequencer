6d256797d087b22e21c6028bbb3671b567ad573a
Ocean Hillman <oceanhillman@gmail.com>
2024-07-17 07:54:09 -0400

    Create header and footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Metronome from "../components/Metronome"
 import Playlist from "@/components/Playlist";
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center">
+      <Header />
       <Playlist />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+    return (
+        <header className="py-12">
+            <h1 className="font-roboto font-bold text-5xl">
+                Metronome Sequencer
+            </h1>
+        </header>
+    )
+}
