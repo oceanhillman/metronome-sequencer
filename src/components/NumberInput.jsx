0a155cb92fdfd6111dcb4904fb876ac6af258de4
Ocean Hillman <oceanhillman@gmail.com>
2024-07-17 17:05:00 -0400

    Fix issue: inputs should have a name

--- src/components/NumberInput.jsx
+++ src/components/NumberInput.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import MinusIcon from "/public/minus.svg";
 import PlusIcon from "/public/plus.svg";
 
-export default function NumberInput( {value, min, max, onChange, } ) {
+export default function NumberInput( {name, value, min, max, onChange, } ) {
 
     const handleIncrement = () => {
         if (value < max) {
@@ -22,13 +22,14 @@ export default function NumberInput( {value, min, max, onChange, } ) {
                 <Image src={MinusIcon} alt="Minus icon" className="w-4 h-4"/>
             </button>
 
-            <input 
-                className="no-spinner mx-2 w-16 bg-gray-900 border border-gray-700 text-gray-300 rounded-md text-center py-1 focus:border-blue-400 focus:ring-0 focus:shadow-outline-blue"
-                type="number" 
-                value={value} 
-                min={min}
-                max={max}
-                onChange={onChange} 
+            <input
+              name={name}
+              className="no-spinner mx-2 w-16 bg-gray-900 border border-gray-700 text-gray-300 rounded-md text-center py-1 focus:border-blue-400 focus:ring-0 focus:shadow-outline-blue"
+              type="number" 
+              value={value} 
+              min={min}
+              max={max}
+              onChange={onChange} 
             />
 
             <button onClick={handleIncrement} className="bg-gray-900 border border-gray-800 text-gray-200 w-8 h-8 flex justify-center items-center rounded-full hover:bg-blue-700 hover:border-blue-500 hover:text-gray-50">
