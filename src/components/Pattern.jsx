9b863471b505615619985a2ebf33f0443a0db374
Ocean Hillman <oceanhillman@gmail.com>
2024-07-16 23:27:32 -0400

    Complete basic functionality

--- src/components/Pattern.jsx
+++ src/components/Pattern.jsx
@@ -1,21 +1,43 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useState } from 'react';
 
 const Pattern = forwardRef((props, ref) => {
-    const { id, dataGrid, bpm, timeSignature, numMeasures } = props;
+    const { dataGrid, patternData, handleUpdatePattern, handleClickDelete } = props;
+
+    const [inputData, setInputData] = useState(
+        {
+            beatsPerMeasure: patternData.beatsPerMeasure,
+            bpm: patternData.bpm,
+            numMeasures: patternData.numMeasures,
+        });
+
+    function handleUpdate(attribute, value) {
+        setInputData((prev) => ({
+            ...prev,
+            [attribute]: value,
+        }));
+        handleUpdatePattern(patternData.id, attribute, value)
+    }
 
     return (
-        <div data-grid={dataGrid} key={ref} className="grid grid-cols-12 grid-rows-2 rounded-xl w-full bg-red-100 border-2 border-black text-black">
-            <div className="col-span-1 row-span-2">
-                <div className="font-roboto font-bold justify-center items-center flex">
-                    <input className="w-full text-center" type="number" value={timeSignature[0]} onChange={() => {}} />
+        <div data-grid={{...dataGrid, isResizable: false}} key={ref} className="flex flex-row rounded-md h-[100px] w-full content-between justify-end bg-red-800 text-black font-roboto">
+        <p>Clone</p><p>Delete</p>
+            <div className="absolute w-full h-[75%] rounded-b-md bottom-0 bg-white/70">
+                <div className="flex">
+                    <div className="w-1/4">
+                        Beats per measure <input className="w-12" type="number" value={inputData.beatsPerMeasure} onChange={(e) => handleUpdate('beatsPerMeasure', Number(e.target.value))} />
+                    </div>
+                    <div className="w-1/4">
+                        BPM: <input className="w-12" type="number" value={inputData.bpm} onChange={(e) => handleUpdate('bpm', Number(e.target.value))} />
+                    </div>
+                    <div className="w-1/4">
+                        x<input className="w-12" type="number" value={inputData.numMeasures} onChange={(e) => handleUpdate('numMeasures', Number(e.target.value))} /> Measures
+                    </div>
+                    <div className="w-1/4">
+                        <button onMouseDown={(event) => handleClickDelete(event, patternData.id)} className="bg-slate-700 text-white text-sm px-1 py-1 rounded">
+                            x
+                        </button>
+                    </div>
                 </div>
-                <div className="font-roboto font-bold justify-center items-center flex">
-                    <input className="w-full text-center" type="number" value={timeSignature[1]} onChange={() => {}} />
-                </div>
-            </div>
-            <div className="col-span-11 row-span-2 flex">
-                <div className="w-1/2">a</div>
-                <div className="w-1/2">b</div>
             </div>
         </div>
     );
