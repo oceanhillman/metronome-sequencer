a151168a7cf0e4afe9570c9041ae5f5400f8bdbb
Ocean Hillman <oceanhillman@gmail.com>
2024-07-17 17:00:41 -0400

    Add delete and clone

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,45 +1,15 @@
 'use client'
 
-
 import "../app/globals.css";
-import { useState, useEffect, useRef } from "react"
-import Metronome from "@/components/Metronome"
-import GridLayout from "react-grid-layout"
+import { useState } from "react"
 import { WidthProvider, Responsive } from 'react-grid-layout';
 import Pattern from "@/components/Pattern"
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
-function generatePatternId() {
-    return String(Math.round(Date.now() + Math.random()));
-}
-
 export default function Playlist(props) {
-    const { playlistData, handleUpdatePlaylist } = props;
-    const [sequencing, setSequencing] = useState(false);
+    const { playlistData, handleUpdatePlaylist, handleClonePattern } = props;
     const [layoutData, setLayoutData] = useState([]);
-
-    // Clear all patterns from playlist
-    function handleClear() {
-        handleUpdatePlaylist([]);
-    }
-
-    // Delete a single pattern from playlist
-    function handleClickDelete(event, id) {
-        event.stopPropagation();
-        handleUpdatePlaylist((prevItems) => prevItems.filter(item => item.id !== id));
-    }
-
-    // Trigger metronome to play playlist
-    function handleClickPlay() {
-        if (playlistData.length > 0)
-            setSequencing(!sequencing);
-    }
-
-    // Callback for when playlist has finished playing
-    function handleSequenceEnd() {
-        setSequencing(false);
-    };
 
     // Sort the layout by the y value (and x if necessary)
     function sortLayout(layout) {
@@ -61,17 +31,6 @@ export default function Playlist(props) {
         handleUpdatePlaylist(newPlaylistOrder);
     }
 
-    // Add new pattern with default values
-    function initalizeNewPattern() {
-        const newPattern = {
-            id: generatePatternId(),
-            bpm: 120,
-            beatsPerMeasure: 4,
-            numMeasures: 4,
-        };
-        handleUpdatePlaylist((prev) => [...prev, newPattern]);
-    }
-
     // Callback function for onLayoutChange 
     function handleLayoutChange(layout) {
         const sortedLayout = sortLayout(layout);
@@ -89,14 +48,27 @@ export default function Playlist(props) {
         );
     }
 
+    // Delete a single pattern from playlist
+    function handleClickDelete(event, id) {
+        event.stopPropagation();
+        handleUpdatePlaylist((prevItems) => prevItems.filter(item => item.id !== id));
+    }
+
+    function handleClickClone(event, pattern) {
+        event.stopPropagation();
+        handleClonePattern(pattern);
+    }
+
     return (
         <ResponsiveGridLayout
-            className="layout mx-96"
+            className="layout"
             layout={layoutData}
             cols={{xxl:1, xl:1, lg:1, md:1, sm:1, xs:1, xxs:1}}
             rowHeight={100}
-            width={""}
+            width={"100%"}
             onLayoutChange={(layout) => handleLayoutChange(layout)}
+            draggableHandle=".handle" // This makes the first div draggable
+            draggableCancel=".no-drag" // This makes the second div non-draggable
         >
             {playlistData.map(pattern => (
                 <div key={pattern.id} className="h-full">
@@ -106,6 +78,7 @@ export default function Playlist(props) {
                         patternData={pattern}
                         handleUpdatePattern={handleUpdatePattern}
                         handleClickDelete={handleClickDelete}
+                        handleClickClone={handleClickClone}
                     />
                 </div>
             ))}
