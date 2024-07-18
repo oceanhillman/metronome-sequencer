4a908ad6f31f5be8f35fcf83bac7e20efd7afef5
Ocean Hillman <oceanhillman@gmail.com>
2024-07-18 08:40:40 -0400

    Add cutive mono font

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
       fontFamily: {
         roboto: ['Roboto', 'sans-serif'],
         manrope: ['Manrope', 'sans-serif'],
+        'cutive-mono': ['"Cutive Mono"', 'monospace'],
       },
     },
   },
