54f0d829fe9d6077667d4170415c9f5983e66d17
Ocean Hillman <oceanhillman@gmail.com>
2024-07-19 20:26:28 -0400

    Create endpoint

--- src/app/api/user/addUser/route.js
+++ src/app/api/user/addUser/route.js
@@ -1,28 +1,36 @@
-// app/api/addUser/route.js
-import { Pool } from 'pg';
-
-// Create a pool instance with your PostgreSQL connection details
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-});
-
-export async function POST(request) {
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+ 
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const user_id = searchParams.get('user_id');
+  const email = searchParams.get('email');
+  const name = searchParams.get('name');
+ 
   try {
-    const { email, name } = await request.json();
-
-    // Validate input
-    if (!email || !name) {
-      return new Response('Missing email or name', { status: 400 });
+    if (!user_id || !email || !name) {
+      return NextResponse.json({ error: 'User_id, email, and name are required' }, { status: 400 });
     }
 
-    // Insert user into the database
-    const query = 'INSERT INTO users (email, name) VALUES ($1, $2)';
-    const values = [email, name];
-    await pool.query(query, values);
+    // Prevent duplicate user_id
+    const existingUser_id = await sql`SELECT 1 FROM Users WHERE User_id = ${user_id} LIMIT 1`;
+    if (existingUser_id.rowCount > 0) {
+      return NextResponse.json({ error: 'User_id already exists' }, { status: 400 });
+    }
 
-    return new Response('User added successfully', { status: 200 });
+    // Prevent duplicate email
+    const existingEmail = await sql`SELECT 1 FROM Users WHERE Email = ${email} LIMIT 1`;
+    if (existingEmail.rowCount > 0) {
+      return NextResponse.json({ error: 'Email already exists' }, { status: 400 });
+    }
+
+    // Insert user
+    await sql`INSERT INTO Users (User_id, Email, Name) VALUES (${user_id}, ${email}, ${name})`;
   } catch (error) {
-    console.error('Error adding user:', error);
-    return new Response('Internal Server Error', { status: 500 });
+    return NextResponse.json({ error }, { status: 500 });
   }
+ 
+  // Respond with user
+  const user = await sql`SELECT 1 FROM Users WHERE User_id = ${user_id} LIMIT 1`;
+  return NextResponse.json({ user }, { status: 200 });
 }
