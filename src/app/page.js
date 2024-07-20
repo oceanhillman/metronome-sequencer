6767cfade7e923784152e38b8479ca0b9d46d60f
Ocean Hillman <oceanhillman@gmail.com>
2024-07-20 17:33:29 -0400

    Remove unused imports

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,13 @@
-import Image from "next/image";
 import Editor from "@/components/Editor";
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
-import { auth, currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
-
-
-  
-  return (
-    <main className="flex min-h-screen flex-col items-center">
-      <Header />
-      <Editor />
-      <Footer />
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center">
+            <Header />
+            <Editor />
+            <Footer />
+        </main>
+    );
 }
