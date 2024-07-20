937321c4d378145950cecf066931d28358b26d7c
Ocean Hillman <oceanhillman@gmail.com>
2024-07-20 17:33:55 -0400

    Create addSong endpoint

---
+++ src/app/api/songs/addSong/route.js
@@ -0,0 +1,49 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+
+export async function POST(request) {
+  try {
+    // Parse JSON body from the request
+    const { user_id, title, created_at, last_saved, playlist, layout } = await request.json();
+
+    // Validate required fields
+    if (!user_id || !title || !playlist || !layout) {
+      return NextResponse.json({ error: 'Important field missing' }, { status: 400 });
+    }
+
+    // Check user exists
+    const existingUser = await sql`
+      SELECT EXISTS (
+        SELECT 1 
+        FROM users 
+        WHERE user_id = ${user_id}
+      ) AS user_exists;
+    `;
+    if (!existingUser.rows[0].user_exists) {
+      return NextResponse.json({ error: "User doesn't exist." }, { status: 400 });
+    }
+
+    // Prevent duplicate title
+    const existingTitle = await sql`
+      SELECT EXISTS (
+        SELECT 1 
+        FROM songs 
+        WHERE user_id = ${user_id} AND title = ${title}
+      ) AS song_exists;
+    `;
+    if (existingTitle.rows[0].song_exists) {
+      return NextResponse.json({ error: 'User already has a song with that title.' }, { status: 400 });
+    }
+
+    // Insert song
+    await sql`
+      INSERT INTO Songs (User_id, Title, Created_at, Last_saved, Playlist, Layout) 
+      VALUES (${user_id}, ${title}, ${created_at}, ${last_saved}, ${playlist}, ${layout})
+    `;
+
+    return NextResponse.json("Successfully added song", { status: 200 });
+
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
