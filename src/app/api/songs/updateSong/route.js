e3abdd161cb6559342f6671fb48f0802060b71e2
Ocean Hillman <oceanhillman@gmail.com>
2024-07-21 07:42:00 -0400

    Add validation to endpoint

--- src/app/api/songs/updateSong/route.js
+++ src/app/api/songs/updateSong/route.js
@@ -2,7 +2,7 @@ import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
 
-export const PATCH = withApiAuthRequired(async function getSongById(request) {
+export const PATCH = withApiAuthRequired(async function PATCH(request) {
   try {
     const { id, user_id, title, last_saved, playlist, layout } = await request.json();
 
@@ -11,8 +11,32 @@ export const PATCH = withApiAuthRequired(async function getSongById(request) {
       }
     
     const { user } = await getSession(request);
-    if (user?.sub !== user_id) {
+    if (user.sub !== user_id) {
         return NextResponse.json({ error: 'Unable to save: user does not own the specified song' }, { status: 403 });
+    }
+    console.log(user);
+
+    let parsedPlaylist;
+    let parsedLayout;
+
+    try {
+      parsedPlaylist = JSON.parse(playlist);
+      parsedLayout = JSON.parse(layout);
+    } catch (error) {
+      console.error('JSON Parse Error:', error);
+      return NextResponse.json({ error: 'Invalid input: playlist and layout must be valid JSON strings' }, { status: 400 });
+    }
+
+    if (!id || typeof id !== 'number' ||
+        !user_id || typeof user_id !== 'string' ||
+        !title || typeof title !== 'string' ||
+        !last_saved || typeof last_saved !== 'string' ||
+        !playlist || !Array.isArray(parsedPlaylist) ||
+        !layout || !Array.isArray(parsedLayout)) {
+
+      console.error('Validation Error:', { id, user_id, title, last_saved, playlist, layout });
+
+      return NextResponse.json({ error: 'Unable to save: incorrect types for required fields' }, { status: 400 });
     }
 
     const query = `
