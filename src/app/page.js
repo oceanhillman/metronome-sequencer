4d82fbd0351c3fef69ca4756d37944434c1dc916
Ocean Hillman <oceanhillman@gmail.com>
2024-07-21 19:07:28 -0400

    Remove unecessary async

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Editor from "@/components/Editor";
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 
-export default async function Home() {
+export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center">
             <Header />
