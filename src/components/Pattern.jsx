033008693554dc1098fae16a84f750d7cd7c390d
Ocean Hillman <oceanhillman@gmail.com>
2024-07-22 14:16:33 -0400

    Feature: hide pattern controls while performing

--- src/components/Pattern.jsx
+++ src/components/Pattern.jsx
@@ -30,14 +30,36 @@ const Pattern = forwardRef((props, ref) => {
         event.stopPropagation();
         startFromPattern(patternData.id)
     }
+
+    function Buttons() {
+        if (!performing) {
+            return (
+                <div className="flex h-[24px] items-center space-x-1 ml-auto mr-2">
+                    <button onMouseDown={(event) => handleClickPlay(event)} className="w-[20px] h-[20px]">
+                        <FaPlay />
+                    </button>
+                    <button onMouseDown={(event) => handleClickClone(event, patternData)} className="w-[20px] h-[20px]">
+                        <Image src={CloneIcon} alt="Clone icon" className="w-auto h-auto"/>
+                    </button>
+                    <button onMouseDown={(event) => handleClickDelete(event, patternData.id)} className="w-[20px] h-[20px]">
+                        <Image src={DeleteIcon} alt="Delete icon" className="w-auto h-auto"/>
+                    </button>
+                </div>
+            );
+        } else {
+            return (
+                <div clasName="ml-auto"></div>
+            );
+        }
+    }
     
 
     return (
-        <div data-grid={{...dataGrid, isResizable: false}} key={ref} className={`handle cursor-move flex flex-row rounded-md h-full w-full content-between justify-center items-start 
+        <div data-grid={{...dataGrid, isResizable: false}} key={ref} className={`handle cursor-move flex flex-row rounded-md h-full w-full content-between justify-center 
             ${currentPatternId === patternData.id ? "bg-arsenic" : "bg-muted-blue"} text-black`}>
 
             <input 
-                className="bg-inherit ml-2 text-cultured font-sans text-sm mt-[2px]"
+                className="bg-inherit ml-2 text-cultured font-sans text-sm mt-[2px] self-start mr-auto"
                 type="text"
                 placeholder="Pattern Name"
                 value={inputData.name}
@@ -46,17 +68,7 @@ const Pattern = forwardRef((props, ref) => {
             />
             <Image src={DragHandleIcon} alt="Drag handle icon" className="absolute w-auto h-auto"/>
 
-            <div className="flex h-[24px] items-center space-x-1 ml-auto mr-2">
-                <button onMouseDown={(event) => handleClickPlay(event)} className="w-[20px] h-[20px]">
-                    <FaPlay />
-                </button>
-                <button onMouseDown={(event) => handleClickClone(event, patternData)} className="w-[20px] h-[20px]">
-                    <Image src={CloneIcon} alt="Clone icon" className="w-auto h-auto"/>
-                </button>
-                <button onMouseDown={(event) => handleClickDelete(event, patternData.id)} className="w-[20px] h-[20px]">
-                    <Image src={DeleteIcon} alt="Delete icon" className="w-auto h-auto"/>
-                </button>
-            </div>
+            <Buttons />
 
             <div className={`no-drag cursor-auto absolute w-full h-[80%] rounded-b-md bottom-0 
             ${currentPatternId === patternData.id ? "bg-subtle-gray" : "bg-eerie-black"}`}>
