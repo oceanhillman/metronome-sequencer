f3989324a823c76a464e87c745cb1832f25b45a8
Ocean Hillman <oceanhillman@gmail.com>
2025-02-21 12:35:47 -0500

    Update pro features page design

--- src/app/get-pro/GetPro.jsx
+++ src/app/get-pro/GetPro.jsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
 import { useUser } from '@auth0/nextjs-auth0/client';
-import { Container, Row, Col, Card, Button, ListGroup } from 'react-bootstrap';
+import Image from 'next/image';
 import { FaCheck } from 'react-icons/fa';
 import { isSubscribed } from '@lib/api';
 
@@ -9,23 +9,24 @@ const GetPro = () => {
     const { user, error: authError, isLoading } = useUser();
     const [subscribed, setSubscribed] = useState(false);
     
-    useEffect(() => {
-        if (isLoading || !user) {
-            setSubscribed(false);
-        } else if (user) {
-            async function getSubscriptionStatus() {
-                const subscriptionStatus = await isSubscribed(user.email);
-                setSubscribed(subscriptionStatus);
-            }
-        
-            getSubscriptionStatus();
-        }
-    }, [user, isLoading]);
+
+
+    const CheckIcon = (props) => (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24px"
+          height="24px"
+          fill="currentColor" // Inherits text color
+          className={props.className}
+        >
+          <path d="M 12 2 C 6.486 2 2 6.486 2 12 C 2 17.514 6.486 22 12 22 C 17.514 22 22 17.514 22 12 C 22 10.874 21.803984 9.7942031 21.458984 8.7832031 L 19.839844 10.402344 C 19.944844 10.918344 20 11.453 20 12 C 20 16.411 16.411 20 12 20 C 7.589 20 4 16.411 4 12 C 4 7.589 7.589 4 12 4 C 13.633 4 15.151922 4.4938906 16.419922 5.3378906 L 17.851562 3.90625 C 16.203562 2.71225 14.185 2 12 2 z M 21.292969 3.2929688 L 11 13.585938 L 7.7070312 10.292969 L 6.2929688 11.707031 L 11 16.414062 L 22.707031 4.7070312 L 21.292969 3.2929688 z"/>
+        </svg>
+      );
 
 
     const handleCheckout = async () => {
         if (!user) {
-            // If user is not authenticated, redirect to login
             window.location.href = '/api/auth/login';
             return;
         }
@@ -52,32 +53,62 @@ const GetPro = () => {
     };
 
     return (
-        <Container className="mt-5">
-            <h1 className="text-center mb-4 text-cultured">The ultimate practice tool for musicians.</h1>
-            <Row className="justify-content-center">
-                <Col md={8}>
-                    <Card className="!bg-chinese-black border-2 !border-muted-blue">
-                        <Card.Body>
-                            <Card.Title className="text-center !text-cyan !text-3xl">Upgrade to Pro</Card.Title>
-                            <Card.Text className="text-center !text-cultured">
-                                <span className="text-2xl text-bold">$1.99</span> / Month
-                            </Card.Text>
-                            <ListGroup variant="flush" className="!bg-chinese-black">
-                                <ListGroup.Item className="!bg-inherit !text-cultured !border-arsenic !flex !flex-row !items-center"><FaCheck className="mr-2" />Save your projects to your song library</ListGroup.Item>
-                                <ListGroup.Item className="!bg-inherit !text-cultured !border-arsenic !flex !flex-row !items-center"><FaCheck className="mr-2" />Share your songs with your friends (free users gain read-only access)</ListGroup.Item>
-                                <ListGroup.Item className="!bg-inherit !text-cultured !border-arsenic !flex !flex-row !items-center"><FaCheck className="mr-2" />Save others' songs and edit them however you want</ListGroup.Item>
-                                <ListGroup.Item className="!bg-inherit !text-cultured !border-arsenic !flex !flex-row !items-center"><FaCheck className="mr-2" />Support me, a poor person</ListGroup.Item>
-                            </ListGroup>
-                            <div className="d-flex justify-content-center mt-4">
-                                <Button onClick={handleCheckout} variant="primary" size="lg" className="!bg-persian-pink !text-chinese-black !font-medium !border-none">
-                                    Get Pro
-                                </Button>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
+        <div className="text-cultured font-roboto min-h-screen bg-eerie-black ">
+            <section className="bg-eerie-black pt-8 px-4 md:pt-28 lg:pb-4 w-screen lg:w-full">
+                <h1 className="text-left lg:text-center text-4xl tracking-tight leading-none sm:text-5xl">
+                    Perfect practice begins here
+                </h1>
+            </section>
+            <section className="bg-eerie-black">
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 p-4">
+                    <div className="bg-arsenic p-4 shadow">
+                        <h2 className="font-bold my-2">Free</h2>
+                        <h2><span className="font-bold">$0 </span><span className="text-sm">USD /month</span></h2>
+                        <a href="/api/auth/login" className="w-full mt-4 xl:mt-0 inline-flex items-center justify-center px-5 py-3 shadow text-base no-underline font-medium text-center text-eerie-black bg-cultured hover:bg-primary-800 focus:ring-4 focus:ring-primary-300">
+                            Create account
+                        </a>
+                        <li className="mt-8 mb-4 flex">
+                            <span className="mr-2 inline-block">
+                                <CheckIcon />
+                            </span>Create metronome sequences in the song editor
+                        </li>
+                        <li className="my-4 flex">
+                            <span className="mr-2 inline-block">
+                                <CheckIcon />
+                            </span>Save one song to your song library
+                        </li>
+                        <li className="my-4 flex">
+                            <span className="mr-2 inline-block">
+                                <CheckIcon />
+                            </span>View others' shared songs as read-only
+                        </li>
+                    </div>
+                    <div className="bg-arsenic p-4 border-1 border-persian-pink shadow">
+                        <h2 className="font-bold my-2 text-persian-pink">Pro</h2>
+                        <h2><span className="font-bold">$1.99 </span> <span className="!text-sm">USD /month</span></h2>
+                        <button onClick={handleCheckout} className="w-full mt-4 xl:mt-0 inline-flex items-center justify-center px-5 py-3 shadow text-base no-underline font-medium text-center text-persian-pink border-1 border-persian-pink hover:bg-arsenic focus:ring-4 focus:ring-gray-100">
+                            Subscribe
+                        </button> 
+                        <p className="mt-4 font-bold text-lg">All of our free features, plus:</p>
+                        <li className="my-4 flex">
+                            <span className="mr-2 inline-block">
+                                <CheckIcon />
+                            </span>Save unlimited songs to your song library
+                        </li>
+                        <li className="my-4 flex">
+                            <span className="mr-2 inline-block">
+                                <CheckIcon />
+                            </span>Share your songs with others
+                        </li>
+                        <li className="my-4 flex">
+                            <span className="mr-2 inline-block">
+                                <CheckIcon />
+                            </span>Copy others' songs to your library and make your own edits
+                        </li>
+                    </div>
+                </div>
+            </section>
+        </div>
     );
 };
 
