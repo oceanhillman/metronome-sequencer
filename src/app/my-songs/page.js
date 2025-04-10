c124f6125a620aff17b1be71fe16f66dc1afeb60
Ocean Hillman <oceanhillman@gmail.com>
2025-04-10 11:49:21 -0400

    fix: attempt fix vercel prerender error by forcing dynamic

--- src/app/my-songs/page.js
+++ src/app/my-songs/page.js
@@ -1,5 +1,7 @@
 import MySongs from "./MySongs";
 
+export const dynamic = 'force-dynamic';
+
 function MySongsPage() {
     return (
         <div className="flex min-h-screen flex-col items-center">
