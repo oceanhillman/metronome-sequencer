3425bb6b0469bfa9e78b6110e154d2ec158aab99
Ocean Hillman <oceanhillman@gmail.com>
2025-05-22 16:41:03 -0400

    feat: add vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import "./globals.css";
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
-
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   title: "Metronome Sequencer",
@@ -37,6 +37,7 @@ export default function RootLayout({ children }) {
                 {children}
             </main>
             <Footer />
+             <Analytics />
         </body>
         </html>
     </UserProvider>
